a26e3118e02b7249ed73e963a516bc0e58544da2
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-18 01:29:25 -0300

    Incluindo servlet contador

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 4. Rode o comando `java -jar target/endorsed/webapp-runner.jar --port $PORT target/MavenWebProject-1.0-SNAPSHOT.war`
     * Substituindo o `$PORT` pela porta que você desejar usar....
     * Por exemplo: se for usar a porta 3333 para testar basta acessar http://localhost:3333
+
+# Adição no projeto
+* Foi adicionado o servlet Contador
+    * Esse servlet testa o conceito de que por padrão cada servlet tem apenas 1 instância e que a mesma é compartilhada por todas as threads.
---
+++ src/main/java/hello/ContadorServlet.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author @igor-lisboa
+ */
+@WebServlet("/contador")
+public class ContadorServlet extends HttpServlet {
+    private int contador = 0; // variavel de instância
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        contador++; // a cada requisição a mesma variável é incrementada
+
+        // recebe o writer
+        PrintWriter out = response.getWriter();
+
+        // escreve o texto
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Contador agora é: " + contador);
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
